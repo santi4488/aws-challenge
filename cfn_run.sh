a661ceaa09c6c447d7fa2ac9513b18f1b50290af
Eamonn Comerford <eamonn.comerford@rackspace.com>
2019-12-02 09:10:56 -0600

    chore: cleanup CFN run script

--- cfn_run.sh
+++ cfn_run.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# bucket_name=cc-ops-13860
 stack_name=aws-challenge
 
-# aws cloudformation package --template-file template.yaml --output-template-file serverless-output.yaml --s3-bucket $bucket_name
-# aws cloudformation deploy --template-file serverless-output.yaml --stack-name $stack_name --capabilities CAPABILITY_NAMED_IAM
 aws cloudformation deploy --template-file template.yaml --stack-name $stack_name --capabilities CAPABILITY_NAMED_IAM
